c5c39f0e8a28010bf8fb2195ab55325dc6227fcb
Ethan Virgil <101530219+E-Virgil@users.noreply.github.com>
2023-12-04 22:24:36 -0500

    Update flightpricing.py

--- flightpricing.py
+++ flightpricing.py
@@ -1,10 +1,10 @@
 import requests
 import json
 
-# Your API key should go here
+
 api_key = 'sh428739766321522266746152871799'
 
-# The data payload
+
 data = {
     "query": {
         "market": "US",
@@ -29,23 +29,21 @@ data = {
     "nearbyAirports": True
 }
 
-# Headers
+
 headers = {
     'Content-Type': 'application/json',
     'x-api-key': api_key
 }
 
-# The endpoint URL
+
 url = 'https://partners.api.skyscanner.net/apiservices/v3/flights/live/search/create'
 
-# Make the POST request
+
 response = requests.post(url, headers=headers, json=data)
 
 
-# Check the response
+
 if response.ok:
-    # If response is OK, print the content
     print(response.json())
 else:
-    # If response is not OK, print the error
     print(f"Failed to retrieve data: {response.status_code}, {response.text}")
