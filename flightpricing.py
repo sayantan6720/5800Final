7efd80a2437b398f0e0fb9a3b9b131849c247c63
E-Virgil <virgil.e@northeastern.edu>
2023-12-05 15:50:21 -0500

    Added some comments to flightpricing.py

--- flightpricing.py
+++ flightpricing.py
@@ -1,8 +1,10 @@
 import requests
 import json
 
+# public api key we found on skyscanner documentation
 api_key = 'sh428739766321522266746152871799'
 
+# this is the actual payload, using for testing
 data = {
     "query": {
 
@@ -30,18 +32,22 @@ data = {
     }
 }
 
+# the headers
 headers = {
     'Content-Type': 'application/json',
     'x-api-key': api_key
 }
 
+# url to get the create serach
 url = 'https://partners.api.skyscanner.net/apiservices/v3/flights/live/search/create'
 
+# the actual post
 response = requests.post(url, headers=headers, json=data)
 
 
+# simple if else to help with debugging
 if response.ok:
     print(response.json())
 else:
-    print(f"Failed to retrieve data: {response.status_code}, {response.text}")
+    print(f"Error when retrieving data: {response.status_code}, {response.text}")
 
