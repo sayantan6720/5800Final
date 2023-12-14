941b4d50abdcf41ca76857de0bd347be09b7c422
E-Virgil <virgil.e@northeastern.edu>
2023-12-13 20:12:06 -0500

    Had to move View into Controller for flask to work properly

--- Controller/test.py
+++ Controller/test.py
@@ -1,13 +1,6 @@
 # this file was written, in its entirety at this point, by Sayantan Datta, I (Ethan Virgil) uploaded it as he was having trouble pushing to this repo
 from flask import Flask, render_template, request
 
-# ethan virgil implemented the calls to the CheapestFlightSearcher class
-from Model.CheapestFlightSearcher import CheapestFlightSearcher
-from Model.adjacencymatrix import AdjacencyMatrix
-matrix = AdjacencyMatrix("Model/Flight Data - Sheet1.csv")
-airport_code_to_index_dict = {flight_label: index for index, flight_label in enumerate(matrix.get_vertices())}
-searcher = CheapestFlightSearcher(matrix.get_GHG_matrix(), airport_code_to_index_dict)
-
 app = Flask(__name__)
 
 
