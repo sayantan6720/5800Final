53378f5473b55b7daa5b68ae674fe1c0c6b3078c
Sayantan Datta <63536152+sayantan6720@users.noreply.github.com>
2023-12-13 21:28:30 -0500

    Update test.py

--- Controller/test.py
+++ Controller/test.py
@@ -1,35 +1,19 @@
-# this file was written, in its entirety at this point, by Sayantan Datta, I (Ethan Virgil) uploaded it as he was having trouble pushing to this repo
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
-  # Check if the form was submitted
-  if request.method == 'POST':
-    # Get the selected source and destination from the form data
-    source = request.form.get('source')
-    destination = request.form.get('destination')
+    source = None
+    destination = None
 
-    # Store the values in a Python variable
-    flight_data = {
-      'source': source,
-      'destination': destination,
-    }
+    if request.method == 'POST':
+        source = request.form.get('source')
+        destination = request.form.get('destination')
 
-    # Print the selected source and destination for debugging purposes
-    print(f"Selected source: {flight_data['source']}")
-    print(f"Selected destination: {flight_data['destination']}")
+    return render_template('UI.html', source=source, destination=destination)
 
-    # Further processing with flight_data (e.g., search for flights)
-    # ... (your flight search logic here)
-
-    # Return the template with a message if desired
-    return render_template('UI.html', message="Flight data received!")
-  else:
-    # Render the form if it wasn't submitted
-    return render_template('UI.html')
-
+if __name__ == "__main__":
+    app.run(debug=True)
 if __name__ == "__main__":
   app.run(debug=True)
